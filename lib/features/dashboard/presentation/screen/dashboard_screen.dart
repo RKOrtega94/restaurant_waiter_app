268fbc2655e8ae971f1fb70c5bd9a87fcab75d8b
RKOrtega <rkortega1994@gmail.com>
2025-02-13 18:56:50 -0500

    Implement navigation rail in dashboard layout and update main layout structure

--- lib/features/dashboard/presentation/screen/dashboard_screen.dart
+++ lib/features/dashboard/presentation/screen/dashboard_screen.dart
@@ -13,13 +13,57 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MainLayout(
-      child: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            title: Text('Test'),
-            centerTitle: false,
-          )
+    return MainLayout(
+      child: Row(
+        spacing: 5,
+        children: [
+          NavigationRail(
+            backgroundColor: Theme.of(context).colorScheme.surface,
+            extended: true,
+            elevation: 8,
+            indicatorColor: Colors.transparent,
+            leading: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: CircleAvatar(
+                radius: 100,
+                backgroundImage: NetworkImage(
+                  'https://avatars.githubusercontent.com/u/23533486?v=4',
+                ),
+              ),
+            ),
+            destinations: const [
+              NavigationRailDestination(
+                icon: Icon(Icons.home),
+                label: Text('Home'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.restaurant),
+                label: Text('Orders'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.settings),
+                label: Text('Settings'),
+              ),
+            ],
+            selectedIndex: 0,
+            onDestinationSelected: (int index) {},
+          ),
+          Expanded(
+            child: CustomScrollView(
+              slivers: [
+                const SliverAppBar(
+                  leading: Icon(Icons.menu),
+                ),
+                SliverFillViewport(
+                  delegate: SliverChildListDelegate(
+                    [
+                      const Placeholder(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
--- lib/shared/layout/main/main_layout.dart
+++ lib/shared/layout/main/main_layout.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 ///
 /// This is the main layout of the application which will be used to wrap the
 /// main content of the application.
+///
+/// - [child] - The child widget to be displayed in the main layout.
 class MainLayout extends StatelessWidget {
   const MainLayout({
     required this.child,
@@ -16,7 +18,53 @@ class MainLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBody: true,
-      body: child,
+      body: SizedBox(
+        height: MediaQuery.sizeOf(context).height,
+        width: MediaQuery.sizeOf(context).width,
+        child: Row(
+          spacing: 10,
+          children: [
+            NavigationRail(
+              backgroundColor: Theme.of(context).colorScheme.surface,
+              extended: true,
+              elevation: 8,
+              indicatorColor: Colors.transparent,
+              unselectedIconTheme: const IconThemeData(color: Colors.grey),
+              selectedIconTheme: const IconThemeData(color: Colors.blue),
+              selectedLabelTextStyle: const TextStyle(
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+              leading: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: CircleAvatar(
+                  radius: 20,
+                  backgroundImage: NetworkImage(
+                    'https://avatars.githubusercontent.com/u/23533486?v=4',
+                  ),
+                ),
+              ),
+              destinations: const [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.restaurant),
+                  label: Text('Orders'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.settings),
+                  label: Text('Settings'),
+                ),
+              ],
+              selectedIndex: 0,
+              onDestinationSelected: (int index) {},
+            ),
+            const Expanded(child: Placeholder())
+          ],
+        ),
+      ),
     );
   }
 }
